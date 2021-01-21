5248cd79f445abd1098470bd92bfefb0c6f492e6
mklau <matthewklau@fas.harvard.edu>
2021-01-21 15:57:07 -0500

    Adds report rendering to plan.R

--- R/plan.R
+++ R/plan.R
@@ -5,14 +5,18 @@ plan <- drake_plan(
                      full.names = TRUE),
   data.raw = load_data(files),
   Data = clean_data(data.raw),
-## Contour Plots
-  plot_contour(Data),
+## Contour Plot
+  fig.cp = plot_contour(Data),
 ## Histogram plot  
-  aph_hist = ggplot(Data, aes(x=Year)) +
-    geom_histogram(binwidth = 28, fill="darkcyan", col="Black", size=1, alpha=0.65) + 
-    theme(panel.background = element_rect(fill="darkseagreen1")) +
-    labs(title = "Aphaenogaster Observations", y="Number of Observations"),
-   
-  BClim = brick("data/YbrevBC_2.5.grd")
-  
+  ## aph_hist = ggplot(Data, aes(x=Year)) +
+  ##   geom_histogram(binwidth = 28, fill="darkcyan", col="Black", size=1, alpha=0.65) + 
+  ##   theme(panel.background = element_rect(fill="darkseagreen1")) +
+  ##   labs(title = "Aphaenogaster Observations", y="Number of Observations"),
+
+  ## BClim = brick("data/YbrevBC_2.5.grd"),
+## Render report
+  report = rmarkdown::render(
+                          knitr_in("report.Rmd"),
+                          output_format = "pdf_document",
+                          quiet = TRUE)
 )
